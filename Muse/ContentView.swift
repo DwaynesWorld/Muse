80146188f0dcdd3411d39d3fc3d3cfd6ec8c0cfc
DwaynesWorld <kthompson713@gmail.com>
2020-06-27 10:26:08 -0500

    Initial Commit

---
+++ Muse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Muse
+//
+//  Created by Kyle Thompson on 6/27/20.
+//  Copyright © 2020 Kyle Thompson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
